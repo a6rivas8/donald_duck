63a790c78dbdd465f20a660bced1f4b563ceec19
Alexander Rivas <alexander.rivas@live.com>
2019-11-21 16:19:03 -0600

    Initial Commit

---
+++ PA3Team6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PA3Team6
+//
+//  Created by Team6 on 11/21/19.
+//  Copyright © 2019 Team6. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
